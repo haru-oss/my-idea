42d5c61894a6ef9d1f5d91ea40337ddf2d213536
haru-oss <hauyaishungry@gmail.com>
2024-06-11 10:40:23 +0900

    テーブルにjsonデータを展開するところまで書いた。デザインをもっとちゃんとしたい。見やすくしたい。

--- idea/src/App.tsx
+++ idea/src/App.tsx
@@ -1,10 +1,10 @@
 import { FC, useState } from "react";
-
+import { Member } from "./user";
 
 
 export const App:FC=()=> {
 
-  const [inputFile,SetinputFile] =useState<string | null>(null);
+  const [inputFile,SetinputFile] =useState<Member[]>([]);
 
 
   const handleFileChange=(event:React.ChangeEvent<HTMLInputElement>) => {
@@ -19,7 +19,7 @@ export const App:FC=()=> {
 
     fileReader.onload = (e)=> {
       const content = e.target?.result as string;
-      const obj = JSON.parse(content);
+      const obj:Member[] = JSON.parse(content);
       console.log(obj)
       SetinputFile(obj);
     };
@@ -47,14 +47,42 @@ export const App:FC=()=> {
 
          <h1>移籍退会者リスト抽出いたします〜</h1>
 
-        
-
-
-
-
          <input type="file"  style={{margin:"100px"}} onChange={handleFileChange}/>
 
          <button  style={{color:"purple",backgroundColor:"#deafde", padding:"5px",margin:"10px", borderRadius:"999px",cursor:"pointer"}}>change!!</button>
+
+
+         {inputFile.length > 0 ?(
+          <table style={{border:"1px"}}>
+            <thead>
+                <tr>
+                  <th>会員詳細</th>
+                  <th>氏名（名）</th>
+                  <th>氏名（姓）</th>
+                  {/* <th>電話番号</th>
+                  <th>メール</th>
+                  <th>月会費</th> */}
+            </tr>
+            </thead>
+            <tbody>
+              {inputFile.map((file,index)=>(
+                <tr key={index}>
+                  <td>{file["会員詳細"]}</td>
+                  <td>{file["氏名（名）"]}</td>
+                  <td>{file["氏名（姓）"]}</td>
+                  {/* <td>{file["電話番号"]}</td>
+                  <td>{file["メール"]}</td>
+                  <td>{file["月会費"]}</td> */}
+                </tr>
+
+              ))}
+            </tbody>
+          </table>
+
+
+         ):(
+          <p>No data available</p>
+         )}
 
       </div>
 
---
+++ idea/src/user.ts
@@ -0,0 +1,18 @@
+export type Member =  {
+    "会員詳細": string;
+    "氏名（名）": string;
+    "氏名（姓）": string;
+    "メール": string;
+    "セキュリティーキー": string | null;
+    "店舗名": string;
+    "携帯電話番号": string | null;
+    "電話番号": string;
+    "生年月日": string;
+    "性別": string;
+    "会員種類": string;
+    "月会費": string;
+    "有効期限": string | null;
+    "入会日": string | null;
+    "入会ステータス": string | null;
+    "Membrアプリのダウンロード": string;
+  }
